9f3c29031448694807e26b14eb58ebdc9668788b
Carlos <cavu93@hotmail.com>
2020-08-01 12:03:02 -0600

    S e agrego la clase SQLSentencias y se relacionaron las capas

--- Program/DAL/DA.cs
+++ Program/DAL/DA.cs
@@ -1,12 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DAL
 {
-    class DA
+    public class DA
     {
+        #region Atributos 
+        private string cadenaconexion = Properties.Settings.Default.Conexion;
+        private SqlConnection objconexion;
+        #endregion
+
+        #region Constructor
+
+        public DA()
+        {
+            try
+            {
+                objconexion = new SqlConnection(cadenaconexion);
+                this.ABRIR();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                this.CERRAR();
+            }
+        }
+
+        #endregion
+       
+        
+        #region PRIVADOS
+
+        private void ABRIR()
+        {
+            if (objconexion.State == System.Data.ConnectionState.Closed)
+                objconexion.Open();
+        }
+
+        private void CERRAR()
+        {
+            if (objconexion.State == System.Data.ConnectionState.Open)
+                objconexion.Close();
+        }
+        #endregion
+
+
+
+
     }
 }

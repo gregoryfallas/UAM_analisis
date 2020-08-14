1c356d1adec62e50ac090ab18408e4c74cb684a7
eportuguez2019 <elena.portuguez@uamcr.net>
2020-08-14 02:45:28 -0600

    2

--- Program/PL/Pantallas/Adicionales/Frm_Ingreso_Suministros_PL.cs
+++ Program/PL/Pantallas/Adicionales/Frm_Ingreso_Suministros_PL.cs
@@ -17,7 +17,6 @@ namespace PL.Pantallas.Adicionales
 {
     public partial class Frm_Ingreso_Suministros_PL : Form
     {
-        private int ID_Solicitud_Articulos = 0;
         private int ID_Articulo_Proveedor = 0;
         private int ID_Solicitud_Compra = 0;
         private int Cantidad = 0;
@@ -30,37 +29,51 @@ namespace PL.Pantallas.Adicionales
         {
             DAL.Entidades.SOLICITUD_ARTICULOS sa = new SOLICITUD_ARTICULOS();
             InitializeComponent();
-            //listingreso_suministros = new list<SOLICITUD_ARTICULOS>();
-            crear_ingreso_suministros(sa);
-            cargardatos_ingreso_sumi();
+            //cargar textbox 
+           
         }
-
-        public void crear_ingreso_suministros(SOLICITUD_ARTICULOS lstParametros)
+        public void cargar_arProveedor(SOLICITUD_ARTICULOS lstParametros)
         {
-                this.tb_cant = null;
-                this.tb_descrip = null;
-                this.tb_idarticulo = null;
-                this.tb_idformulario = null;
-                this.tb_idproveedor = null;
-                this.dataGrid_ingresosumi.DataSource = null;
+            List<ARTICULOS_PROVEEDOR> ingreso_articuloproveedor = Suministros_BLL.SP_CREAR_ingreso_suministro(in_sumi);
 
-            this.dataGrid_ingresosumi.DataSource = Suministros_BLL.SP_CREAR_ingreso_suministro(lstParametros);
-                this.tb_descrip = null;
-                this.tb_idarticulo = null;
-                this.tb_idformulario = null;
-                this.tb_idproveedor = null;
-                this.dataGrid_ingresosumi.DataSource = null;
-
-                this.dataGrid_ingresosumi.Refresh();
+            this.tb_cant.DataSource = lstParametros;
+            this.tb_cant = Suministros_BLL.SP_CREAR_ingreso_suministro(lstParametros);
+            this.tb_cant.Refresh();
         }
 
-        public void cargardatos_ingreso_sumi()
+        public void cargar_SolCompra(SOLICITUD_ARTICULOS lstParametros)
         {
-            List<ARTICULOS_PROVEEDOR> aRTICULOS_PROVEEDORs = in_sumi.ingreso_articuloproveedor;
-            List<SOLICITUD_COMPRA> sOLICITUD_COMPRAs = in_sumi.ingreso_Solicitudcompra;
-            List<SOLICITUD_ARTICULOS> sOLICITUD_ARTICULOs = in_sumi.ingreso_Cantidad;
-            List<DETALLE_ARTICULOS> dETALLE_ARTICULOs = in_sumi.ingreso_Descripcion;
+            List<SOLICITUD_COMPRA> ingreso_articuloproveedor = Suministros_BLL.SP_CREAR_ingreso_suministro(in_sumi);
+
+            this.tb_idformulario.DataSource = lstParametros;
+            this.tb_idformulario = Suministros_BLL.SP_CREAR_ingreso_suministro(lstParametros);
+            this.tb_idformulario.Refresh();
         }
+        
+        
+       
+        //public void crear_ingreso_suministros(SOLICITUD_ARTICULOS lstParametros)
+        //{
+        //        
+        //        this.dataGrid_ingresosumi.DataSource = null;
+
+        //    this.dataGrid_ingresosumi.DataSource = Suministros_BLL.SP_CREAR_ingreso_suministro(lstParametros);
+        //        this.tb_descrip = null;
+        //        this.tb_idarticulo = null;
+        //        this.tb_idformulario = null;
+        //        this.tb_idproveedor = null;
+        //        this.dataGrid_ingresosumi.DataSource = null;
+
+        //        this.dataGrid_ingresosumi.Refresh();
+        //}
+
+        //public void cargardatos_ingreso_sumi()
+        //{
+        //    List<ARTICULOS_PROVEEDOR> aRTICULOS_PROVEEDORs = in_sumi.ingreso_articuloproveedor;
+        //    List<SOLICITUD_COMPRA> sOLICITUD_COMPRAs = in_sumi.ingreso_Solicitudcompra;
+        //    List<SOLICITUD_ARTICULOS> sOLICITUD_ARTICULOs = in_sumi.ingreso_Cantidad;
+        //    List<DETALLE_ARTICULOS> dETALLE_ARTICULOs = in_sumi.ingreso_Descripcion;
+        //}
 
 
 
